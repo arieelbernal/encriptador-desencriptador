86619c5f349ef386e29e75716701c66899151df4
arieelbernal <arieelbernal@gmail.com>
2022-05-08 18:15:31 -0300

    actualizacion y terminacion de desafio

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,7 @@ window.addEventListener("load", function(){
     let input = document.getElementById("entrada");
     let encriptador = document.getElementById("encriptar");
     let desencriptador = document.getElementById("desencriptar");
+    let boxRespuesta = document.getElementById("respuesta");
     let llaves= {
         a: "ai",
         e: "enter",
@@ -9,6 +10,21 @@ window.addEventListener("load", function(){
         o: "ober",
         u: "ufat",
     };
+    let copiarTexto = () => {
+        let btnCopiar = document.getElementById("btn");
+        btnCopiar.addEventListener("click", ()=>{navigator.clipboard.writeText(document.getElementById('render').innerHTML)
+            .then(() => {
+            console.log("El texto se copio en el portapapeles")
+        })
+            .catch(err => {
+            console.log('Se produjo un error al copiar', err);
+        });
+        });
+    }
+    let renderizar = (texto) => {
+        boxRespuesta.innerHTML= `<p id="render">${texto}</p><button id="btn">Copiar</button>`;
+        copiarTexto();
+    }
     let encriptar = (texto) => {
         texto = texto.toLowerCase();
         let resultado=""
@@ -34,20 +50,24 @@ window.addEventListener("load", function(){
                     break;
             }
         }
-        return resultado;
+        return resultado[0].toUpperCase() + resultado.slice(1);
     };
     let desencriptar = (texto) => {
         for (let i = 0; i < 5; i++) {
             texto = texto.replaceAll(Object.values(llaves)[i],`${Object.keys(llaves)[i]}`);
         }
-        return texto;
+        return texto[0].toUpperCase() + texto.slice(1);
     };
 
     encriptador.addEventListener("click", ()=>{
-        console.log(encriptar(input.value));
+        if(input.value){
+            renderizar(encriptar(input.value));
+        }
     });
 
     desencriptador.addEventListener("click", ()=>{
-        console.log(desencriptar(input.value));
+        if(input.value){
+            renderizar(desencriptar(input.value));
+        }
     });
 });
